29df8a52fb9b88c20834a5528878afa83eacebb8
Saqib <prosaqib2022@gmail.com>
2024-06-20 04:19:52 +0500

    plugin header added

--- woocommerce-modified-sort/woocommerce-modified-sort.php
+++ woocommerce-modified-sort/woocommerce-modified-sort.php
@@ -1,3 +1,11 @@
+<?php
+/*
+Plugin Name: WooCommerce Sort by Last Modified
+Description: Sort WooCommerce products by last modified date on shop and category/tag pages.
+Version: 1.0
+Author: ProSaqib
+*/
+
 function sort_products_by_last_modified($query) {
     if (!is_admin() && $query->is_main_query() && (is_shop() || is_product_category() || is_product_tag() || is_post_type_archive('product'))) {
         $query->set('orderby', 'modified');
@@ -6,4 +14,3 @@ function sort_products_by_last_modified($query) {
 }
 
 add_action('pre_get_posts', 'sort_products_by_last_modified');
-add_action('elementor/query/woocommerce-products', 'sort_products_by_last_modified');
